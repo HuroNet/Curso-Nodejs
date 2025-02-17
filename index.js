05a38987cac30ca6a767a459c27bba2fd27eb5cc
Huron <charlie.walls97@gmail.com>
2025-02-17 09:25:36 -0500

    format document

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const fs = require('fs/promises');
+const fs = require("fs/promises");
 
-async function main(){
-    try {
-        const data = await fs.readFile("input.txt", 'utf-8');
-        console.log("File content:", data);
-    } catch (error) {
-        console.error("Error reading file:", error);  // ✅ Corrección aquí
-    }
+async function main() {
+  try {
+    const data = await fs.readFile("input.txt", "utf-8");
+    console.log("File content:", data);
+  } catch (error) {
+    console.error("Error reading file:", error); // ✅ Corrección aquí
+  }
 
-    try {
-        await fs.writeFile("Output.txt", "Hello world!", "utf-8");
-        console.log("file created successfully");
-    } catch (error) {
-        console.error("Errpr creating fil;e", error);        
-    }
-
-     
+  try {
+    await fs.writeFile("Output.txt", "Hello world!", "utf-8");
+    console.log("file created successfully");
+  } catch (error) {
+    console.error("Errpr creating fil;e", error);
+  }
 }
 
 main();
