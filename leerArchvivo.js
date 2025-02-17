25eca151a892d041bcf1b86811c3efc98abed205
Huron <charlie.walls97@gmail.com>
2025-02-17 07:32:41 -0500

    add new things

--- leerArchvivo.js
+++ leerArchvivo.js
@@ -1,1 +1,13 @@
 const fs = require('fs/promises');///es uin modulo en version de promesas
+const EventEmitter = require('events');
+
+class FileReadEmiter extends EventEmitter{
+    async readFile(file){
+        try {
+            const data = await fs.readFile(file, 'utf8');
+            this.emit('read',file,data)
+        } catch (error) {
+            
+        }
+    }
+}
